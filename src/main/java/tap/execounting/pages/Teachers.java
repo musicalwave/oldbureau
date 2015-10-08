aca1e249b87c9befb4b25df07729e89f5d14a225
razdvapoka <razdvapoka@gmail.com>
2015-10-08 13:10:19 +0300

    Teachers list is one-column grid now

--- src/main/java/tap/execounting/pages/Teachers.java
+++ src/main/java/tap/execounting/pages/Teachers.java
@@ -2,26 +2,31 @@ package tap.execounting.pages;
 
 import java.util.List;
 
-import org.apache.tapestry5.annotations.Import;
-import org.apache.tapestry5.annotations.InjectPage;
+import org.apache.tapestry5.ComponentResources;
+import org.apache.tapestry5.beaneditor.BeanModel;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
+import org.apache.tapestry5.services.BeanModelSource;
 import tap.execounting.dal.mediators.interfaces.TeacherMed;
 import tap.execounting.entities.Teacher;
 
-@Import(stylesheet = "context:/css/teacherslist.css")
 public class Teachers {
 
 	@Inject
 	private TeacherMed tMed;
 
-	@InjectPage
-	private TeacherPage page;
+	@Inject
+	private BeanModelSource source;
 
-	@SuppressWarnings("unused")
-	private Object onActionFromTLink(Teacher context) {
-		page.setup(context);
-		return page;
+    @Inject
+	private ComponentResources resources;
+
+	private BeanModel<Teacher> model;
+
+	void pageLoaded() {
+		model = source.createDisplayModel(Teacher.class, resources.getMessages());
+		model.include("name");
+		model.get("name").label("Преподаватель");
 	}
 
 	public List<Teacher> getAll() {
@@ -35,4 +40,9 @@ public class Teachers {
 	public void setUnit(Teacher unit) {
 		tMed.setUnit(unit);
 	}
+
+	public BeanModel<Teacher> getModel(){
+		return model;
+	}
+
 }
